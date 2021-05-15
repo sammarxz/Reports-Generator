4295914dc324cf2cea39cdc84c21f43edb4ea759
Sam Marxz <sammarxzz@gmail.com>
2021-05-15 12:08:28 -0300

    calc frequency of each food

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,6 +1,17 @@
 defmodule ReportsGenerator do
   alias ReportsGenerator.Parser
 
+  @available_foods [
+    "açaí",
+    "churrasco",
+    "esfirra",
+    "pizza",
+    "prato_feito",
+    "hambúrguer",
+    "pastel",
+    "sushi"
+  ]
+
   def build(filename) do
     filename
     |> Parser.parse_file()
@@ -11,9 +22,19 @@ defmodule ReportsGenerator do
 
   def fetch_higher_value(report), do: Enum.max_by(report, fn {_key, value} -> value end)
 
-  defp sum_values([id, _food_name, price], report) do
-    Map.put(report, id, report[id] + price)
+  defp sum_values([id, food_name, price], %{"foods" => foods, "users" => users} = report) do
+    users = Map.put(users, id, users[id] + price)
+    foods = Map.put(foods, food_name, foods[food_name] + 1)
+
+    show_report(report, users, foods)
   end
 
-  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+  defp report_acc do
+    foods = Enum.into(@available_foods, %{}, &{&1, 0})
+    users = Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+
+    %{"users" => users, "foods" => foods}
+  end
+
+  defp show_report(report, users, foods), do: %{report | "users" => users, "foods" => foods}
 end
